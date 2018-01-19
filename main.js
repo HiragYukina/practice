6a78fddb77801af67a7ac6b1507751e461259b38
yukina <g.taiki.0909@gmail.com>
2018-01-19 15:46:47 +0900

    カメラ機能追加ver1

--- main.js
+++ main.js
@@ -1,26 +1,37 @@
 var layers,
   map_width,
   map_height,
-  tile_rect = [],
+  tile_rect = {
+    x: 0,
+    y: 0
+  },
   tile = [],
   images = []
 var canvas,
   ctx,
   image
+var camera = {
+  x: 0,
+  y: 0
+}
 var gamemode = "game"
 window.addEventListener("DOMContentLoaded", init)
 
 function init() {
+
+  //キャンパス要素
   canvas = document.getElementById("maincanvas")
   ctx = canvas.getContext("2d")
+  //タイル読み込み
   image = new Image();
   image.src = "images/map_images/map1.png"
-
+  //マップの生成
   mapload(function(event) {
     load_map(event);
     stageSetup()
     requestAnimationFrame(updata)
-  });
+  })
+
 }
 function mapload(onload) {
   //JSON読み込み
@@ -41,26 +52,37 @@ function load_map(event) {
   //マップ高さ
   map_height = mainmap.height
   //タイル
-  tile_rect[0] = mainmap.tilewidth
-  tile_rect[1] = mainmap.tileheight
-
+  tile_rect.x = mainmap.tilewidth
+  tile_rect.y = mainmap.tileheight
 }
 function stageSetup() {
+  //画像読み込み完了
   image.addEventListener("load", renderSteage())
 }
 function renderSteage() {
+
+  const screen_width = 640
+  const screen_height = 480
+
+  const startTileX = Math.floor((camera.x - screen_width / 2) / tile_rect.x)
+  const startTileY = Math.floor((camera.y - screen_height / 2) / tile_rect.y)
+
+  //レイヤー
   for (var i = 0; i < layer.length; i++) {
-    for (var y = 0; y < map_height; y++) {
-      for (var x = 0; x < map_width; x++) {
+    //縦幅
+    for (var y = startTileY; y < map_height; y++) {
+      //横幅
+      for (var x = startTileX; x < map_width; x++) {
+
         var id = layer[i].data[x + y * map_width] - 1
-        var image_x = (id % 16) * 32
-        var image_y = Math.floor(id / 16) * 32
-        ctx.drawImage(image, image_x, image_y, 32, 32, x * 32, y * 32, 32, 32);
+        var image_x = (id % 16) * tile_rect.x
+        var image_y = Math.floor(id / 16) * tile_rect.y
+        //         タイル画像、トリミング　ｘ、ｙ、 タイル幅ｘ、ｙ、　　　　    描画開始位置　ｘ、　ｙ　　　　　　　　描画タイルｘ、ｙ、
+        ctx.drawImage(image, image_x, image_y, tile_rect.x, tile_rect.y, x * tile_rect.x, y * tile_rect.y, tile_rect.x, tile_rect.y);
       }
     }
   }
 }
-
 function updata() {
   requestAnimationFrame(updata)
   render()
