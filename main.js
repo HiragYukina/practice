aaf59cceca326a10379832eb4dc24b989db46018
yukina <g.taiki.0909@gmail.com>
2018-02-21 20:33:17 +0900

    マップの拡大とスクロールの可変化

--- main.js
+++ main.js
@@ -72,6 +72,8 @@ function renderSteage() {
 
   const screen_width = 640
   const screen_height = 480
+  const widthMax = map_width * tile_rect.x
+  const heightMax = map_height * tile_rect.y
 
   const screenLeft = camera.x - screen_width / 2
   const screenTop = camera.y - screen_height / 2
@@ -80,13 +82,13 @@ function renderSteage() {
   const startTileY = Math.floor(screenTop / tile_rect.y)
 
   //レイヤー
-  if (camera.x >= 320 && camera.y >= 240 && camera.x <= 1536 - 320 && camera.y <= 1152 - 240) {
+  if (camera.x >= screen_width / 2 && camera.y >= screen_height / 2 && camera.x <= widthMax - (screen_width / 2) && camera.y <= heightMax - (screen_height / 2)) {
 
     for (var i = 0; i < layer.length; i++) {
       //縦幅
-      for (var y = 0; y < (screen_height / 32) + 1; y++) {
+      for (var y = 0; y < (screen_height / tile_rect.y) + 1; y++) {
         //横幅
-        for (var x = 0; x < (screen_width / 32) + 1; x++) {
+        for (var x = 0; x < (screen_width / tile_rect.x) + 1; x++) {
 
           const index = startTileX + x + (startTileY + y) * map_width
           if (index < 0 || index >= layer[i].data.length) {
@@ -107,17 +109,17 @@ function renderSteage() {
   }
   //画面端の当たり判定
   function flame() {
-    if (camera.x <= 320) {
-      camera.x = 320;
+    if (camera.x <= screen_width / 2) {
+      camera.x = screen_width / 2;
     }
-    if (camera.y <= 240) {
-      camera.y = 240;
+    if (camera.y <= screen_height / 2) {
+      camera.y = screen_height / 2;
     }
-    if (camera.x >= 1536 - 320) {
-      camera.x = 1536 - 320;
+    if (camera.x >= widthMax - (screen_width / 2)) {
+      camera.x = widthMax - (screen_width / 2);
     }
-    if (camera.y >= 1152 - 240) {
-      camera.y = 1152 - 240;
+    if (camera.y >= heightMax - (screen_height / 2)) {
+      camera.y = heightMax - (screen_height / 2);
     }
     renderSteage()
   }
